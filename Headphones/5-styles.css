2d033aa1a8237860bac6a0042e9df9a076ca6d59
Shwanemariti <m.mariti@lab.clicklearning.org>
2024-09-29 20:13:45 +0200

    Create 5-styles.css

---
+++ Headphones/5-styles.css
@@ -0,0 +1,120 @@
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body {
+    font-family: 'Source Sans Pro', sans-serif;
+    color: #333;
+    line-height: 1.6;
+}
+
+header {
+    background-image: url('images/headphones_hero_1.jpg');
+    background-size: cover;
+    background-position: center;
+    padding: 60px 20px;
+    color: white;
+    text-align: center;
+}
+
+.logo img {
+    width: 150px;
+    height: auto;
+}
+
+nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: relative; /* Ensure positioning works correctly */
+}
+
+.nav-links {
+    list-style: none;
+    display: flex;
+    gap: 15px;
+}
+
+.nav-links li a {
+    color: white;
+    text-decoration: none;
+    font-size: 0.75rem;
+    font-weight: bold;
+}
+
+.nav-links li a:hover {
+    color: #FF6565;
+}
+
+.hero-content {
+    margin-top: 100px;
+    flex-wrap: wrap;
+}
+
+.hero-content h1 {
+    font-size: 2rem;
+    margin-bottom: 20px;
+    margin-top: 250px;
+    font-weight: bold;
+}
+
+.hero-content button {
+    width: 200px;
+    padding: 15px;
+    background-color: #FF6565;
+    color: white;
+    font-size: 1rem;
+    border: none;
+    border-radius: 50px;
+    cursor: pointer;
+    font-family: Source Sans Pro, sans-serif;
+    font-weight: bold;
+    transition: background-color 0.3s ease;
+    margin-bottom: 40px;
+}
+
+.hamburger {
+    display: none;
+    font-size: 2rem;
+    cursor: pointer;
+    position: absolute; /* Position the hamburger */
+    left: 20px; /* Align to the left */
+    top: 20px; /* Adjust the top position */
+    background: none; /* Remove background */
+    border: none; /* Remove border */
+}
+
+.menu-toggle {
+    display: none; /* Hide the checkbox */
+}
+
+@media (max-width: 768px) {
+    .nav-links {
+        display: none; /* Hide links by default */
+        flex-direction: column;
+        background-color: none(7, 22, 41, 0.9); /* Semi-transparent background */
+        position: absolute;
+        top: 60px;
+        left: 10px;
+        width: 90%;
+        padding: 20px;
+        border-radius: 5px;
+    }
+
+    .menu-toggle:checked +headphone  .nav-links {
+        display: flex; /* Show the menu when the checkbox is checked */
+    }
+
+    .hamburger {
+        display: block; /* Show headphone icon in mobile */
+    }
+
+    .logo img {
+        position: absolute;
+        left: 50%;
+        top: 10px;
+        transform: translateX(-50%);
+    }
+}
